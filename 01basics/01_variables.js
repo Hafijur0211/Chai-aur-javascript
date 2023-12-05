8de712af59dfd0b904f04f0e6d1cdf9ae2c01f34
hafijur <hafijur0211@gmail.com>
2023-12-05 22:16:52 +0530

    let

---
+++ 01basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 1232
+let accountEmail = "hafijur0211@gamil.com"
+var accountPassword = "1232"
+accountCity = "Bokaro"
+let accountState;
+
+// accountId = 232 // not allowed 
+
+accountEmail = "hafijur09@gamil.com"
+accountPassword = "989"
+accountCity = "Ranchi"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Prefer not to use var
+because of issues in block scope and function scope
+*/
