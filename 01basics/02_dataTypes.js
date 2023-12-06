730f378aa93e43fec9c935b2489973169c679c96
hafijur <hafijur0211@gmail.com>
2023-12-06 08:58:01 +0530

    datatypes

---
+++ 01basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"Use strict"; //  treat all JS code as newer versions
+
+// alert(3 + 3); // we are using node js not browser
+
+console.log(3 + 4);
+
+console.log("Hafijur");
+
+let name = "hafijur";
+let age = 23;
+let isLogged = false;
+
+// number => 2 to power 54
+// bigint
+// string=> ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "hafijur");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
