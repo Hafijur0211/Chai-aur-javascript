a03f0691723ab751fa5f2bf77e4675defc29aaf7
hafijur <hafijur0211@gmial.com>
2023-12-19 21:36:23 +0530

    Array in javascript

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,40 @@
+// arrays
+
+const myArr = [0, 1, 2, 3, 4, 5];
+
+const myHeorse = ["shaktiman", "naagraj"];
+
+const myArray2 = new Array(1, 2, 3, 4);
+
+// console.log(myArray[0]);
+
+// Arrays Methods
+
+// myArray.push(6)
+// myArray.push(7)
+// myArray.pop()
+
+// myArray.unshift(9)
+// myArray.shift()
+
+// console.log(myArray.includes(9))
+// console.log(myArray.indexOf(3))
+
+// const newArray = myArray.join()
+
+// console.log(myArray)
+// console.log(newArray)
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3);
+
+
+console.log(myn1);
+console.log("B ", myArr)
+
+const myn2 = myArr.splice(1, 3);
+console.log("C ", myArr)
+console.log(myn2);
