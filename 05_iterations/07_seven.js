15037c4b993b395267ebccab908da93ef1c92f6c
hafijur <hafijur0211@gmial.com>
2024-01-18 11:21:57 +0530

    filter map and reduce method

---
+++ 05_iterations/07_seven.js
@@ -0,0 +1,19 @@
+const myNumb = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const newNums = myNumb.map((num)=> num +10)
+
+// const newNums = myNumb.map((num) => {
+//     return num + 10;
+// });
+
+// console.log(newNums);
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// chaining methods
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const newNums = myNums.map((num) => num * 10).map((num) => num + 1).filter((num)=> num >= 40);
+
+console.log(newNums);
