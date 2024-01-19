0221b27f9345e9ef701029a6117e498bbf9c6737
hafijur <hafijur0211@gmial.com>
2024-01-19 19:13:50 +0530

    Dom intro

---
+++ 06_dom/script.js
@@ -0,0 +1,100 @@
+// console.log('hello')
+
+// const id = document.getElementById('title')
+// console.log(id)
+
+// const id = document.getElementById('title').id
+// console.log(id)
+
+// const id = document.getElementById('title').className
+// console.log(id)
+
+// const id = document.getElementById('title').getAttribute
+// console.log(id)
+
+// const id = document.getElementById('title').getAttribute('id')
+// console.log(id)
+
+// const id = document.getElementById('title').getAttribute('class')
+// console.log(id)
+
+// const id = document.getElementById('title').setAttribute('class', 'test')
+// console.log(id)
+
+// const title = document.getElementById('title').id
+// console.log(title)
+
+// title.style.backgroundColor ='green'
+// title.style.padding = '15px'
+// title.style.borderRadius = '16px'
+
+// const id = title.textContent
+// console.log(id)
+
+// const id = title.innerHTML
+// console.log(id)
+
+// const id = title.innerText
+// console.log(id)
+
+// const id = title.textContent
+// console.log(id)
+
+// const id = document.getElementsByClassName('heading')
+// console.log(id)
+
+// const id = document.querySelector('h1')
+// console.log(id)
+
+// const id = document.querySelector('h2')
+// console.log(id)
+
+// const id = document.querySelector('#title')
+// console.log(id)
+
+// const id = document.querySelector('.heading')
+// console.log(id)
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// const id = document.querySelector('input[type="password"]')
+// console.log(id)
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// const ul = document.querySelector("ul");
+// ul.querySelector("li");
+// console.log(ul);
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// const turnGreen = ul.querySelector("li");
+// turnGreen.style.backgroundColor = "green";
+// turnGreen.innerText = "five"
+// console.log(turnGreen);
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// const tempLiList = document.querySelectorAll("li");
+// tempLiList[0].style.color = "green";
+// console.log(tempLiList)
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// const myH1 = document.querySelectorAll('h1')
+// myH1[0].style.color = "green";
+// console.log(myH1)
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// const classItem = document.getElementsByClassName("list-item");
+// console.log(classItem);
+
+// const arr = Array.from(classItem)
+// console.log(arr);
+
+// const forEachValue = arr.forEach(function (li) {
+//     li.style.color = 'orange';
+// })
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
